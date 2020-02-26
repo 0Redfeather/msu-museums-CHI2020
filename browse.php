e0987243f15940eec8adad9b008fd3e09eba3e65
zach <franc230@msu.edu>
2020-02-26 17:43:24 -0500

    Messing around with site

---
+++ browse.php
@@ -0,0 +1,29 @@
+<!doctype html>
+
+API URL: https://kora.anthropology.msu.edu/projects/7/{API_FUNCTION_URI}
+
+
+<?php
+$API = https://kora.anthropology.msu.edu/projects/7/api/search
+echo $_POST[];
+?>
+
+Record Search
+
+https://kora.anthropology.msu.edu/projects/7/api/search
+ [
+ {
+    "bearer_token": "5e4f0278b9e13",
+    "form": Heritage_Asset_7_16_
+    "return_fields": [“field 1", “field 2", ...],
+ }
+ ]
+
+<?php
+if(isset($_POST['submit'])
+{
+echo("First name: " . $_POST['firstname'] . "<br />\\n");
+echo("Last name: " . $_POST['lastname'] . "<br />\\n");
+}
+?>
+
