f0af71b68bf1253a76658d4887e7c44a3c12280b
zach <franc230@msu.edu>
2020-05-03 09:19:31 -0400

    Updating Search html

---
+++ kora_search_matrix.php
@@ -0,0 +1,66 @@
+// set post fields
+                         $post = [
+                             'username' => 'user1',
+                             'password' => 'passuser1',
+                             'gender'   => 1,
+                         ];
+
+                         $ch = curl_init('http://www.example.com');
+                         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+                         curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+
+                         // execute!
+                         $response = curl_exec($ch);
+
+                         // close the connection, release resources used
+                         curl_close($ch);
+
+                         // do anything you want with your response
+                         var_dump($response);
+
+static function formSearchBuilder($fid,$token,$flags,$fields,$sort,$queries,$qLogic,$index=null,$count=null,$filterCount=null,$fitlerFields=null,$assocFields=null) {
+     $form = array();
+     $form["form"] = $fid;
+     $form["bearer_token"] = $token;
+
+     $form["data"] = in_array("data",$flags) ? in_array("data",$flags) : false;
+     $form["meta"] = in_array("meta",$flags) ? in_array("meta",$flags) : false;
+     $form["size"] = in_array("size",$flags) ? in_array("size",$flags) : false;
+
+     $form["filters"] = in_array("filters",$flags) ? in_array("filters",$flags) : false;
+     if(!is_null($filterCount))
+         $form["filter_count"] = $filterCount;
+     if(is_array($fitlerFields) && empty($fitlerFields))
+         $form["filter_fields"] = "ALL";
+     else
+         $form["filter_fields"] = $fitlerFields;
+
+     $form["assoc"] = in_array("assoc",$flags) ? in_array("assoc",$flags) : false;
+     if(is_array($assocFields) && empty($assocFields))
+         $form["assoc_fields"] = "ALL";
+     else
+         $form["assoc_fields"] = $assocFields;
+     $form["reverse_assoc"] = in_array("reverse_assoc",$flags) ? in_array("reverse_assoc",$flags) : false;
+
+     $form["real_names"] = in_array("real_names",$flags) ? in_array("real_names",$flags) : false;
+     $form["under"] = in_array("under",$flags) ? in_array("under",$flags) : false;
+
+     if(is_array($fields) && empty($fields))
+         $form["return_fields"] = "ALL";
+     else
+         $form["return_fields"] = $fields;
+     if(!empty($sort))
+         $form["sort"] = $sort;
+
+     if(!is_null($index))
+         $form["index"] = $index;
+     if(!is_null($count))
+         $form["count"] = $count;
+
+     $form["queries"] = $queries;
+     if(!is_null($qLogic))
+         $form["logic"] = $qLogic;
+
+     return $form;
+ }
+}
